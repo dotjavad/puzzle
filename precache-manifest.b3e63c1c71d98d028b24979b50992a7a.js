87d50161640e8bca326c1bdf2962f28872b8f85a
Javad <mirjavad@webtamin.com>
2019-11-05 19:12:21 +0330

    Updates

---
+++ precache-manifest.b3e63c1c71d98d028b24979b50992a7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8687ddfefd686fa85ef4434c60440220",
+    "url": "/puzzle/index.html"
+  },
+  {
+    "revision": "58f874645f4a657b6a47",
+    "url": "/puzzle/static/css/main.b87baed3.chunk.css"
+  },
+  {
+    "revision": "1a25a22a958e57a56b38",
+    "url": "/puzzle/static/js/2.ec83f2ca.chunk.js"
+  },
+  {
+    "revision": "58f874645f4a657b6a47",
+    "url": "/puzzle/static/js/main.31441a88.chunk.js"
+  },
+  {
+    "revision": "aaaa606b3086f2a64ff1",
+    "url": "/puzzle/static/js/runtime~main.76477131.js"
+  }
+]);
